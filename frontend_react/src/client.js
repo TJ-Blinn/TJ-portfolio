e52ec4b52fe4c4a25d1b6bd4547984b3d996a566
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2022-06-21 15:59:19 -0400

    [update] added environment variables for pprojectId and token

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -4,10 +4,9 @@ import sanityClient from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = sanityClient({
-  projectId: "glgfncu3",
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: "production",
   apiVersion: "2022-06-21",
   useCdn: true,
-  token:
-    "skjluV8KGvm9Bu7aVemQM96Vb8gwPZjljTD6Y64C1wWfsA1EExKEU8D0uiQS0COsM8zDSbwf99yLybR9kHptpIphVTfM6gdnzUVYzD9not9rUapsKY14BWOcdrIdxpbdcKzWTS5DKmUMgkoVYB4HGo1yX44Z85bWL03e6dGcDYRFPeqrS9Tc",
+  token: process.env.REACT_APP_SANITY_TOKEN,
 });
