0f2898108f501662f975b3cabf72654793ca2923
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2022-06-21 16:30:41 -0400

    [update]add useState and useEffect to populate dynamic data coming from (Sanity) CMS

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -4,15 +4,26 @@ import { motion } from "framer-motion";
 import { images } from "../../constants";
 
 import "./About.scss";
+import { urlFor, client } from "../../client";
 
-const Abouts = [
-  { title: "Front-end Web development", description: "Designing responsive and dynamic apps from wireframe to production!", imgUrl: images.about01 },
-  { title: "Back-end Web development", description: "Database design with NodeJs, SQL, PostgreSQL and, Sanity", imgUrl: images.about02 },
-  { title: "PERN Stack", description: "Full-stack web development with CRUD operations", imgUrl: images.about03 },
-  { title: "Web Animations", description: "Making designs come to life!", imgUrl: images.about04 },
-];
+// const abouts = [
+//   { title: "Front-end Web development", description: "Designing responsive and dynamic apps from wireframe to production!", imgUrl: images.about01 },
+//   { title: "Back-end Web development", description: "Database design with NodeJs, SQL, PostgreSQL and, Sanity", imgUrl: images.about02 },
+//   { title: "PERN Stack", description: "Full-stack web development with CRUD operations", imgUrl: images.about03 },
+//   { title: "Web Animations", description: "Making designs come to life!", imgUrl: images.about04 },
+// ];
 
 const About = () => {
+  // dynamic data coming from (Sanity) CMS
+  const [abouts, setAbouts] = useState([]);
+
+  // only run at the start, once component loads
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => setAbouts(data));
+  }, []);
+
   return (
     // Empty React fragment
     <>
@@ -21,7 +32,7 @@ const About = () => {
       </h2>
 
       <div className="app__profiles">
-        {Abouts.map((about, index) => (
+        {abouts.map((about, index) => (
           <motion.div
             whileInView={{ opacity: 1 }}
             whileHover={{ scale: 1.1 }}
