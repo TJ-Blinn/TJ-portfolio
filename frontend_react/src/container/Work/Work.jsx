2ef8e3693d98b76c87d10f6b8141d3e14b899fbf
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2022-06-22 23:01:13 -0400

    [feature]set up dependencies, state for active filter, and loop for categories. No errors.

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { AiFillEye, AiFillGithub } from "react-icons/ai";
+import { motion } from "framer-motion";
+
+import { AppWrap } from "../../wrapper";
+import { urlFor, client } from "../../client";
 
 import "./Work.scss";
 
 const Work = () => {
-  return <div>Work</div>;
+  const [activeFilter, setActiveFilter] = useState("All");
+
+  // 'pill' styled buttons above the boxes for each portfolio example | categorize the apps
+  const handleWorkFilter = (item) => {};
+  return (
+    <>
+      <h2 className="head-text">
+        My Creative <span>Portfolio</span> section
+      </h2>
+
+      <div className="app__work-filter">
+        {/* loop over categories */}
+        {["UI/UX", "Web App", "Mobile App", "React Js", "All"].map((item, index) => (
+          <div
+            key={index}
+            onClick={() => handleWorkFilter(item)}
+            className={`app__work-filter-item app__flex p-text ${activeFilter === item ? "item-active" : ""}`}
+          ></div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Work;
