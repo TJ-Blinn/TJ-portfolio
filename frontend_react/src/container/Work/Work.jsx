143093af53d5d6c77c73f40e7a93ce65726f0266
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2022-06-24 21:35:03 -0400

    [update]added notes

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -54,25 +54,21 @@ const Work = () => {
               <motion.div
                 whileHover={{ opacity: [0, 1] }}
                 transition={{ duration: 0.25, ease: "easeInOut", staggerChildren: 0.5 }}
-                className="app__work-hover app_flex"
+                className="app__work-hover app__flex"
               >
-                {/* FillEye icon for projectLink*/}
                 <a href={work.projectLink} target="_blank" rel="noreferrer">
-                  <motion.div whileInView={{ scale: [0, 1] }} whileHover={{ scale: [1, 0.9] }} transition={{ duration: 0.25 }} className="app_flex">
+                  <motion.div whileInView={{ scale: [0, 1] }} whileHover={{ scale: [1, 0.9] }} transition={{ duration: 0.25 }} className="app__flex">
                     <AiFillEye />
                   </motion.div>
                 </a>
-
-                {/* Github icon for codeLink */}
                 <a href={work.codeLink} target="_blank" rel="noreferrer">
-                  <motion.div whileInView={{ scale: [0, 1] }} whileHover={{ scale: [1, 0.9] }} transition={{ duration: 0.25 }} className="app_flex">
+                  <motion.div whileInView={{ scale: [0, 1] }} whileHover={{ scale: [1, 0.9] }} transition={{ duration: 0.25 }} className="app__flex">
                     <AiFillGithub />
                   </motion.div>
                 </a>
               </motion.div>
             </div>
 
-            {/* Title and Description of project for each card*/}
             <div className="app__work-content app__flex">
               <h4 className="bold-text">{work.title}</h4>
               <p className="p-text" style={{ marginTop: 10 }}>
