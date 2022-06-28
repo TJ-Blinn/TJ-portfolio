576838d7fd71b7039056783d231c6bd09446ac6c
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2022-06-28 16:34:37 -0400

    [feature]nested map over experiences and then over works

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -7,7 +7,7 @@ import { urlFor, client } from "../../client";
 import "./Skills.scss";
 
 const Skills = () => {
-  const [experience, setExperience] = useState([]);
+  const [experiences, setExperiences] = useState([]);
   const [skills, setSkills] = useState([]);
 
   // fetching from Sanity
@@ -15,8 +15,10 @@ const Skills = () => {
     const query = '*[_type == "experiences"]';
     const skillsQuery = '*[_type == "skills"]';
 
+    // objects inside the array represent years | each year can have multiple experiences
     client.fetch(query).then((data) => {
-      setExperience(data);
+      // console.log(data);
+      setExperiences(data);
     });
     client.fetch(skillsQuery).then((data) => {
       setSkills(data);
@@ -30,13 +32,47 @@ const Skills = () => {
       <div className="app__skills-container">
         <motion.div className="app__skills-list">
           {/* fetch Skills from Sanity CMS and map over them*/}
-          {skills.map((skill) => (
+          {skills?.map((skill) => (
             <motion.div whileInView={{ opacity: [0, 1] }} transition={{ duration: 0.5 }} className="app__skills-item app__flex" key={skill.name}>
               <div className="app__flex" style={{ backgroundColor: skill.bgColor }}>
                 <img src={urlFor(skill.icon)} alt={skill.name} />
               </div>
 
               <p className="p-text">{skill.name}</p>
+            </motion.div>
+          ))}
+        </motion.div>
+
+        {/* loop over all the experiences */}
+        <motion.div className="app__skills-exp">
+          {/* {console.log("----- skills-exp:", experience.works)} */}
+          {experiences.map((experience) => (
+            <motion.div className="app__skills-exp-item" key={experience.year}>
+              <div className="app__skills-exp-year">
+                <p className="bold-text">{experience.year}</p>
+              </div>
+              <motion.div className="app__skills-exp-works">
+                {/* 1st loops of year, then loop over the experiences contained within */}
+                {experience.works.map((work) => (
+                  <>
+                    <motion.div
+                      whileInView={{ opacity: [0, 1] }}
+                      transition={{ duration: 0.5 }}
+                      className="app__skills-exp-work"
+                      data-tip
+                      data-for={work.name}
+                      key={work.name}
+                    >
+                      <h4 className="bold-text">{work.name}</h4>
+                      <p className="p-text">{work.company}</p>
+                    </motion.div>
+
+                    <ReactTooltip id={work.name} effect="solid" arrowColor="#fff" className="skills-tooltip">
+                      {work.desc}
+                    </ReactTooltip>
+                  </>
+                ))}
+              </motion.div>
             </motion.div>
           ))}
         </motion.div>
