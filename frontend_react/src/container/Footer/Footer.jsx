46117b42cedbc79c0f9f2672538d82312ead74f2
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2022-07-06 23:14:42 -0400

    [feature]add footer section for contact card

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
+import { images } from "../../constants";
+import { AppWrap, MotionWrap } from "../../wrapper";
+import { clinet } from "../../client";
 import "./Footer.scss";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <h2 className="head-text">Grab a coffee and chat with me</h2>
+
+      <div className="app__footer-cards">
+        <div className="app__footer-card">
+          <img></img>
+        </div>
+      </div>
+    </>
+  );
 };
 
-export default Footer;
+export default AppWrap(MotionWrap(Footer, "app__footer"), "contact", "app__whitebg");
