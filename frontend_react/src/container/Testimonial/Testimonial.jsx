c0ea766a2550e89e90b46f005338b9eae1051abe
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2022-07-21 14:55:56 -0400

    [fix]resolved issue on sanity studio, testimonials section working properly

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -9,9 +9,13 @@ import { urlFor, client } from "../../client";
 import "./Testimonial.scss";
 
 const Testimonial = () => {
-  const [brands, setBrands] = useState([]);
-  const [testimonials, setTestimonials] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [testimonials, setTestimonials] = useState([]);
+  const [brands, setBrands] = useState([]);
+
+  const handleClick = (index) => {
+    setCurrentIndex(index);
+  };
 
   // fetching from Sanity
   useEffect(() => {
@@ -27,30 +31,40 @@ const Testimonial = () => {
     });
   }, []);
 
-  return <div>Testimonial</div>;
+  // return <div>Testimonial</div>;
 
-  //   return (
-  //     <>
-  //       {testimonials.length && (
-  //         <>
-  //           <div className="app__testimonial-item app__flex">
-  //             {/* ERROR OCCURS ON THIS LINE! currentIndex is undefined */}
+  return (
+    <>
+      {testimonials.length && (
+        <>
+          <div className="app__testimonial-item app__flex">
+            <img src={urlFor(testimonials[currentIndex].imgurl)} alt={testimonials[currentIndex].name} />
+            {/* {console.log("Results of testimonials: ", testimonials)} */}
 
-  //             <img src={urlFor(testimonials[currentIndex].imgurl)} alt={testimonials[currentIndex].name} />
-  //             {console.log("Results of testimonials: ", testimonials)}
-  //             <div className="app__testimonial-content">
-  //               <p className="p-text">{testimonials[currentIndex].feedback}</p>
-  //               <div>
-  //                 <h4 className="bold-text">{testimonials[currentIndex].name}</h4>
-  //                 <h5 className="p-text">{testimonials[currentIndex].company}</h5>
-  //               </div>
-  //             </div>
-  //           </div>
-  //         </>
-  //       )}
-  //     </>
-  //   );
-  // };
+            <div className="app__testimonial-content">
+              <p className="p-text">{testimonials[currentIndex].feedback}</p>
+              <div>
+                <h4 className="bold-text">{testimonials[currentIndex].name}</h4>
+                <h5 className="p-text">{testimonials[currentIndex].company}</h5>
+              </div>
+            </div>
+          </div>
+
+          {/* Move accross different testimonials */}
+          <div className="app__testimonial-btns app__flex">
+            <div className="app__flex" onClick={() => handleClick(currentIndex === 0 ? testimonials.length - 1 : currentIndex - 1)}>
+              <HiChevronLeft />
+            </div>
+
+            <div className="app__flex" onClick={() => handleClick(currentIndex === testimonials.length - 1 ? 0 : currentIndex + 1)}>
+              <HiChevronRight />
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  );
 };
+// };
 
 export default AppWrap(MotionWrap(Testimonial, "app__testimonial"), "testimonial", "app__primarybg");
