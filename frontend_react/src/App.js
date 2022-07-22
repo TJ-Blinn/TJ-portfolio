1043bb307bff61d238e80ad9f3a87d7801409d83
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2022-07-21 23:20:49 -0400

    [update] added resume component

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
-import { Navbar } from "./components";
+import { Navbar, Resume } from "./components";
 import "./App.scss";
 
 const App = () => {
@@ -11,6 +11,7 @@ const App = () => {
       <Header />
       <About />
       <Work />
+      <Resume />
       <Skills />
       <Testimonial />
       <Footer />
