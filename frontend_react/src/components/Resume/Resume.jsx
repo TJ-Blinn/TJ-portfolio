007041903ff58e30dbe58d3fb108766d9346929e
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2022-07-22 22:52:17 -0400

    [update]removed Resume component

--- frontend_react/src/components/Resume/Resume.jsx
+++ frontend_react/src/components/Resume/Resume.jsx
@@ -1,17 +1,18 @@
-import React from "react";
-import "./Resume.scss";
+// import React from "react";
+// import "./Resume.scss";
 
-function Resume({ title, span }) {
-  return (
-    <div className="app__resume-title-mn">
-      <h2 className="app__resume-title-mn">
-        {title}{" "}
-        <b>
-          <span className="app__resume-title-sp">{span}</span>
-        </b>
-      </h2>
-    </div>
-  );
-}
+// function Resume({ icon, title, span }) {
+//   return (
+//     <div className="app__resume-title">
+//       <h2 className="app__resume-title-mn">
+//         {/* <p className="app__resumeTitle-icon">{icon}</p> */}
+//         {title}{" "}
+//         <b>
+//           <span className="app__resume-title-sp">{span}</span>
+//         </b>
+//       </h2>
+//     </div>
+//   );
+// }
 
-export default Resume;
+// export default Resume;
