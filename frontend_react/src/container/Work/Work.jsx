250aff8e83f4245a42c868f8bb98ff53057be267
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2022-08-01 14:22:32 -0400

    [update] small tweak to text size and color under icon

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -71,11 +71,11 @@ const Work = () => {
                 transition={{ duration: 0.25, ease: "easeInOut", staggerChildren: 0.5 }}
                 className="app__work-hover app__flex"
               >
-                <a href={work.projectLink} target="_blank" rel="noreferrer">
+                {/* <a href={work.projectLink} target="_blank" rel="noreferrer">
                   <motion.div whileInView={{ scale: [0, 1] }} whileHover={{ scale: [1, 0.9] }} transition={{ duration: 0.25 }} className="app__flex">
                     <AiFillEye />
                   </motion.div>
-                </a>
+                </a> */}
                 <a href={work.codeLink} target="_blank" rel="noreferrer">
                   <motion.div whileInView={{ scale: [0, 1] }} whileHover={{ scale: [1, 0.9] }} transition={{ duration: 0.25 }} className="app__flex">
                     <AiFillGithub />
